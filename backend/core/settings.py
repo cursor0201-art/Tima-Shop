cf8744be4072c8699c12d6120f851b39947561a5
Futuree <uldashevshuhrat123@gmail.com>
2026-03-13 20:57:44 +0500

    Update backend settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -163,6 +163,9 @@ SIMPLE_JWT = {
 }
 
 STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
