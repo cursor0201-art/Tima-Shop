f7f661a2d3fe32d29f0eaa0873072a98b89d884b
Futuree <uldashevshuhrat123@gmail.com>
2026-03-15 13:34:08 +0500

    fix(backend): use DATABASE_URL for Postgres connection and update requirements

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -83,22 +83,16 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 import dj_database_url
-DB_NAME = os.environ.get('DB_NAME')
-DB_USER = os.environ.get('DB_USER')
-DB_PASSWORD = os.environ.get('DB_PASSWORD')
-DB_HOST = os.environ.get('DB_HOST')
-DB_PORT = os.environ.get('DB_PORT', '5432')
 
-if DB_PORT and DB_HOST:
+db_url = os.environ.get('DATABASE_URL')
+
+if db_url:
     DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': DB_NAME,
-            'USER': DB_USER,
-            'PASSWORD': DB_PASSWORD,
-            'HOST': DB_HOST,
-            'PORT': DB_PORT,
-        }
+        'default': dj_database_url.parse(
+            db_url,
+            conn_max_age=600,
+            ssl_require=True,
+        )
     }
 else:
     DATABASES = {
